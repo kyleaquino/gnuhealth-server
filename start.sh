18f275578168b67228909acc8e72586082af85cc
Kyle Aquino <kshaunaquino@gmail.com>
2023-09-11 03:17:39 +0800

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-echo "[web]" >> /home/gnuhealth/trytond.conf
-echo "listen = *:8000" >> /home/gnuhealth/trytond.conf
-echo "root = /home/gnuhealth/package/" >> /home/gnuhealth/trytond.conf
-
-trytond -c /home/gnuhealth/trytond.conf
