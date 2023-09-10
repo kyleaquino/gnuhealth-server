e4019cae7ed9a5ed86c988fc944012f9d162f6d7
Kyle Aquino <kshaunaquino@gmail.com>
2023-09-11 02:05:09 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 echo "[web]" >> /home/trytond.conf
-echo "listen = *:80" >> /home/trytond.conf
-echo "root = /home/package/" >> /home/trytond.conf
+echo "listen = *:8000" >> /home/trytond.conf
+echo "root = /home/gnuhealth/package/" >> /home/trytond.conf
