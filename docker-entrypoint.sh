7fd81fa86a27a49b713e4c59acda4ecc058e059e
Kyle Aquino <kshaunaquino@gmail.com>
2023-09-13 07:18:25 +0800

    Update and rename docker-entrypoint.sh to entrypoint.sh

--- docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cat $HOME/trytond.conf > $HOME/gnuhealth/tryton/server/config/trytond.conf
-echo "" >> $HOME/gnuhealth/tryton/server/config/trytond.conf
-echo "[database]" >> $HOME/gnuhealth/tryton/server/config/trytond.conf
-echo "uri = $GNUHEALTH_POSTGRES_URL" >> $HOME/gnuhealth/tryton/server/config/trytond.conf
-
-cd $HOME
-./start_gnuhealth.sh
