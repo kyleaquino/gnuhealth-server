968dd30f8cfea5c7a9add708ef385c5748e026ce
Kyle Aquino <kshaunaquino@gmail.com>
2023-09-12 14:25:36 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,12 @@ then
   export $(cat .env | xargs)
 fi
 
-echo "[database]" >> /home/gnuhealth/gnuhealth/tryton/server/config/trytond.conf
-echo "uri = ${GNUHEALTH_POSTGRES_URL}" >> /home/gnuhealth/gnuhealth/tryton/server/config/trytond.conf
+echo "" >> $TRYTOND_CONFIG
+echo "[database]" >>  $TRYTOND_CONFIG
+echo "uri = ${GNUHEALTH_POSTGRES_URL}" >> $TRYTOND_CONFIG
+
+cd $HOME/gnuhealth/tryton/server/trytond-6.0.35/bin
+python ./trytond-admin --all --database=gnuhealth -c $TRYTOND_CONFIG
 
 cd /home/gnuhealth
 ./start_gnuhealth.sh
