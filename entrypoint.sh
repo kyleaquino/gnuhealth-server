41074a0237ab498fae6b97f6e5404eae06ba0afb
Kyle Aquino <kshaunaquino@gmail.com>
2023-09-13 09:25:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "" >> $HOME/gnuhealth/tryton/server/config/trytond.conf
 echo "[database]" >> $HOME/gnuhealth/tryton/server/config/trytond.conf
 echo "uri = $GNUHEALTH_POSTGRES_URL" >> $HOME/gnuhealth/tryton/server/config/trytond.conf
 
+cd $HOME/gnuhealth/tryton/server/trytond-6.0.35/bin/
 python3 ./trytond-admin --all --database=gnuhealth -c $TRYTOND_CONFIG
 
 cd $HOME
